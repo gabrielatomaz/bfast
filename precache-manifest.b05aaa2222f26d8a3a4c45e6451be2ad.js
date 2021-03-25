0c848e467d669007d40d2ef2d07be63fe8c9a3e5
Gabriela Ribeiro <gabriela.ribeiro@zenvia.com>
2021-03-25 15:09:26 -0300

    Updates

---
+++ precache-manifest.b05aaa2222f26d8a3a4c45e6451be2ad.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36b72451512012f3b79cbe3943afc2b",
+    "url": "/bfast/index.html"
+  },
+  {
+    "revision": "6f9f1b0fc5fa7c93d44c",
+    "url": "/bfast/static/css/main.21a6b87c.chunk.css"
+  },
+  {
+    "revision": "06f7733f3fcc504da0d5",
+    "url": "/bfast/static/js/2.ea86baa0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bfast/static/js/2.ea86baa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9f1b0fc5fa7c93d44c",
+    "url": "/bfast/static/js/main.17373869.chunk.js"
+  },
+  {
+    "revision": "20fdd4696fa1a01afd7b",
+    "url": "/bfast/static/js/runtime-main.57c5fcbb.js"
+  },
+  {
+    "revision": "a2b456a310920f740f2f3a4100152347",
+    "url": "/bfast/static/media/calendar-plus-solid.a2b456a3.svg"
+  },
+  {
+    "revision": "fcf4a3178e052db1db530f2fec5fd587",
+    "url": "/bfast/static/media/list-solid.fcf4a317.svg"
+  },
+  {
+    "revision": "6931087129e0daf657a0f08651087ab8",
+    "url": "/bfast/static/media/logo-blue.69310871.svg"
+  },
+  {
+    "revision": "956d5f71bf602d423b8a02e8cd6473f4",
+    "url": "/bfast/static/media/logo-white.956d5f71.svg"
+  },
+  {
+    "revision": "baf369a8ea7746752e2d5d25fe393a7c",
+    "url": "/bfast/static/media/notebook.baf369a8.jpg"
+  },
+  {
+    "revision": "fc96e38b4916654b30e5c024fbbdbfe1",
+    "url": "/bfast/static/media/plus-circle-solid.fc96e38b.svg"
+  },
+  {
+    "revision": "bbb3271eec2b5a3f5c6f1806d55e8e89",
+    "url": "/bfast/static/media/undo-alt-solid.bbb3271e.svg"
+  },
+  {
+    "revision": "024535748273714efb6f6b0046122a76",
+    "url": "/bfast/static/media/user-solid.02453574.svg"
+  }
+]);
